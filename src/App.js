74653ed45055277026bfc2b51f03d4a177a13b40
Tano <martin.marlatto@mavha.com>
2020-10-19 23:33:49 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="images.jpeg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div>
+			<x3d width="600" height="600">
+				<scene >
+        <viewpoint position="-1.94639 1.79771 -2.89271" orientation="0.03886 0.99185 0.12133 3.75685"></viewpoint>
+					<inline nameSpaceName="Model" mapDEFToID="true" url="Deer.x3d"/>
+				</scene>
+			</x3d>
+		</div>
   );
 }
 
